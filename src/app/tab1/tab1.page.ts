1a9c0b4a59032a14fdc07c9c618d95e834a7c105
Davi Milioli <davi.milioli@inforce.com.br>
2024-05-28 15:00:24 -0300

    criação de slide e posição bloco de recomendações

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -6,7 +6,55 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  currentIndex = 0;
+  slideInterval: any;
 
   constructor() {}
+
+  slideImages = [
+    {
+      image: '',
+      description: 'Organize suas tarefas',
+      color: 'primary'
+    },
+    {
+      image: '',
+      description: 'Anote coisas importantes',
+      color: 'light-blue'
+    },
+    {
+      image: '',
+      description: 'Planeje sua semana',
+      color: 'peach'
+    }
+  ];
+
+  ngOnInit() {
+    this.startSlide();
+  }
+
+  ngOnDestroy() {
+    if (this.slideInterval) {
+      clearInterval(this.slideInterval);
+    }
+  }
+
+  previousSlide() {
+    this.currentIndex > 0 ? this.currentIndex-- : this.currentIndex = this.slideImages.length - 1
+  }
+
+  nextSlide() {
+    this.currentIndex < this.slideImages.length - 1 ? this.currentIndex++ : this.currentIndex = 0;
+  }
+
+  goToSlide(index: number) {
+    this.currentIndex = index;
+  }
+
+  startSlide() {
+    this.slideInterval = setInterval(() => {
+      this.nextSlide();
+    }, 3000);
+  }  
 
 }
