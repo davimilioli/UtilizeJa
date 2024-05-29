7baae7584abac8b9712dc20a9fe079f96e563fa9
Davi Milioli <davimilioli2108@gmail.com>
2024-05-29 10:44:41 -0300

    slide de introdução ao App

--- src/app/pages/introducao/introducao.page.ts
+++ src/app/pages/introducao/introducao.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-introducao',
@@ -6,10 +7,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./introducao.page.scss'],
 })
 export class IntroducaoPage implements OnInit {
+  showSlide: boolean = false;
+  currentSlide: number = 0;
+  slideClasses: string[] = ['', 'light-blue', 'peach'];
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  start() {
+    const expander = document.getElementById('background-expander');
+    if (expander) {
+      expander.classList.add('expanding-background');
+
+      expander.addEventListener('animationend', () => {
+        expander.classList.remove('expanding-background');
+        expander.classList.add('fullscreen-background');
+      }, { once: true });
+
+      this.showSlide = true;
+    }
+  
+    console.log(this.showSlide)
+  }
+
+  nextSlide(){
+    if (this.currentSlide < this.slideClasses.length - 1) {
+      this.currentSlide++;
+    } else {
+      this.router.navigate(['/login'])
+    }
+
+    console.log(this.currentSlide);
+  }
+
+  jumpSlide(){
+    this.router.navigate(['/login'])
+  }
+
+  get currentSlideClass() {
+    return this.slideClasses[this.currentSlide];
   }
 
 }
