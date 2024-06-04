ae92a909507b2a15f94630916d26f2d2d69b827a
Davi Milioli <davi.milioli@inforce.com.br>
2024-06-04 11:46:18 -0300

    organização de pasta de services

--- src/app/tab4/tab4.page.ts
+++ src/app/tab4/tab4.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { AuthService } from '../services/auth.service';
+import { AuthService } from '../services/auth/auth.service';
 import firebase from 'firebase/compat/app';
 import { Router } from '@angular/router';
 
