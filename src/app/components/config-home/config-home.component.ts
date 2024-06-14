77623041193ee97e7990ab2eee490ba78526480d
Davi Milioli <davimilioli2108@gmail.com>
2024-06-14 15:38:54 -0300

    funcionalidades config home

--- src/app/components/config-home/config-home.component.ts
+++ src/app/components/config-home/config-home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ItemReorderEventDetail } from '@ionic/angular';
-import { ConfigHomeService } from 'src/app/services/config-home/config-home.service';
+import { Storage } from '@ionic/storage-angular';
 
 @Component({
   selector: 'app-config-home',
@@ -9,34 +9,26 @@ import { ConfigHomeService } from 'src/app/services/config-home/config-home.serv
 })
 export class ConfigHomeComponent implements OnInit {
   @Input() isModalOpen: boolean = false;
-  toolsOrder: any;
+  tools: any[] = []; 
+  configHome: boolean = false; // criar uma função para ativar e desativar
 
-  constructor(private configHomeService: ConfigHomeService) { }
+  constructor(private storage: Storage) { }
 
   async ngOnInit() {
-    const orderedItems = await this.configHomeService.getItems();
-    if (orderedItems.length > 0) {
-      this.tools = orderedItems;
+    await this.storage.create();
+    const storedItems = await this.storage.get('OrderItems');
+    if (storedItems && storedItems.length > 0) {
+      this.tools = storedItems;
+    } else {
+      // criar função para ativar e desativar tool
+      this.tools = [
+        { name: 'Lista de Afazeres', icon: 'checkmark-done-circle', active: false },
+        { name: 'Conversor de PDF', icon: 'document', active: false },
+        { name: 'Bloco de Notas', icon: 'pencil', active: false },
+        { name: 'Consultar CEP', icon: 'search', active: false }
+      ];
     }
   }
-  tools: any[] = [
-    {
-      name: 'Lista de Afazeres',
-      icon: 'checkmark-done-circle'
-    },
-    {
-      name: 'Conversor de PDF',
-      icon: 'document'
-    },
-    {
-      name: 'Bloco de Notas',
-      icon: 'pencil'
-    },
-    {
-      name: 'Consultar CEP',
-      icon: 'search'
-    },
-  ];
 
   setOpen(isOpen: boolean) {
     this.isModalOpen = isOpen;
@@ -49,11 +41,16 @@ export class ConfigHomeComponent implements OnInit {
     const movedItem = this.tools.splice(fromIndex, 1)[0];
     this.tools.splice(toIndex, 0, movedItem);
 
-    await this.configHomeService.saveItems(this.tools);
+    await this.saveOrder();
 
-    console.log('Ordem ', this.tools);
+    console.log('Ordem atualizada', this.tools);
 
     ev.detail.complete();
+  }
+
+  async saveOrder() {
+    await this.storage.set('OrderItems', this.tools);
+    console.log('Ordem salva', this.tools);
   }
 
 }
