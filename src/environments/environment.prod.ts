3e8c592ceb4f33435f9953b9b3915572962a0c38
Davi Milioli <102887880+davimilioli@users.noreply.github.com>
2024-06-28 00:14:27 -0300

    config firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,11 +2,11 @@ export const environment = {
   production: true,
   apiBrasil: 'https://brasilapi.com.br/api',
   firebaseConfig:{
-    apiKey: "",
-    authDomain: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: ""
+    apiKey: "AIzaSyCc2dGYFYd0jrT5cnVOgQjGUUoboTu2sqQ",
+    authDomain: "utilizeja-43df0.firebaseapp.com",
+    projectId: "utilizeja-43df0",
+    storageBucket: "utilizeja-43df0.appspot.com",
+    messagingSenderId: "462930439784",
+    appId: "1:462930439784:web:5009bb2aa87e26a0c155dd"
   }
 };
